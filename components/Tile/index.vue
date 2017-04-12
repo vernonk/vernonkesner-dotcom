8a3b305d0f4af434f0f4e07e584823a19e166e9d
Vernon Kesner <vernonkesner@gmail.com>
2017-04-11 21:58:36 -0400

    Simple Tile component and default content until later move to db/api

---
+++ components/Tile/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="column">
+    <template v-if="contentType === 'consulting'">
+      <h2 class="title">{{ tileContent.consulting.title }}</h2>
+      <p>{{tileContent.consulting.content}}</p>
+    </template>
+    <template v-else-if="contentType === 'development'">
+      <h2 class="title">{{ tileContent.development.title }}</h2>
+      <p>{{tileContent.development.content}}</p>
+    </template>
+    <template v-else-if="contentType === 'training'">
+      <h2 class="title">{{ tileContent.training.title }}</h2>
+      <p>{{tileContent.training.content}}</p>
+    </template>
+    <template v-else>
+      <h2 class="title">{{ title }}</h2>
+      <p class="content">{{content}}</p>
+    </template>
+  </section>
+</template>
+
+<script>
+import Vue from 'vue';
+import tileContent from './tile-content';
+
+export default Vue.component('vk-tile', {
+  props: [ 'title', 'content', 'contentType', 'className' ],
+  data: () => ({
+    tileContent
+  })
+});
+</script>
---
+++ components/Tile/tile-content.js
@@ -0,0 +1,30 @@
+// until i actually setup a db and api to fetch content
+export const consulting = {
+  title: 'Consulting',
+  content: `I provide JavaScript and Front-End consultancy services to companies of all sizes.
+            From how to build a scalable and performant codebase, how to build
+            living pattern libaries and style guides;
+            to how to work better together as a team, grab hold of the design process,
+            and deliver high quality and responsive User Interface and Experience projects.`
+};
+
+export const development = {
+  title: 'Development',
+  content: `I provide development services for projects of all sizes and durations,
+            from both an architecture and implementation perspective. My focus is
+            on how we can build the best product, within the reality of your
+            space, in a way that works. I can join your product team for project
+            kickoff sprints where we can work together to define architecture and workflow,
+            and make sure your teams are self-reliant after I'm gone.`
+};
+
+export const training = {
+  title: 'Training & Workshops',
+  content: `tbd`
+};
+
+export default {
+  consulting,
+  development,
+  training
+};
